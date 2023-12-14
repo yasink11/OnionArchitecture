3c4fe57ac18e088e1f7c9fc5c8f47d1f19460a4d
Yasin Karacam <ysnkrcm146@gmail.com>
2023-12-14 11:50:17 +0300

    Token Service

--- Infrastructure/OnionArchitecture.Infrastructure/Tokens/TokenService.cs
+++ Infrastructure/OnionArchitecture.Infrastructure/Tokens/TokenService.cs
@@ -5,11 +5,12 @@ using OnionArchitecture.Application.Interfaces.Tokens;
 using OnionArchitecture.Domain.Entities;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace OnionArchitecture.Infrastructure.Tokens
 {
-    public class TokenService : ITokenServices
+    public class TokenService : ITokenService
     {
         private readonly UserManager<User> userManager;
         private readonly TokenSettings tokenSettings;
@@ -30,11 +31,11 @@ namespace OnionArchitecture.Infrastructure.Tokens
                 new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
                 new Claim(JwtRegisteredClaimNames.Email,user.Email),
             };
-                foreach (var role in roles)
+            foreach (var role in roles)
             {
                 claims.Add(new Claim(ClaimTypes.Role, role));
             }
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret));
 
             var token = new JwtSecurityToken(
                 issuer: tokenSettings.Issuer,
@@ -44,17 +45,38 @@ namespace OnionArchitecture.Infrastructure.Tokens
                 signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256));
 
             await userManager.AddClaimsAsync(user, claims);
+
             return token;
         }
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomNumber = new byte[64];
+            using var rng= RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
 
-        public ClaimsPrincipal? GetPrincipalFromExpiredToken()
+        public ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token)
         {
-            throw new NotImplementedException();
+            TokenValidationParameters tokenValidationParameters = new()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Secret)),
+                ValidateLifetime = false,
+            };
+            JwtSecurityTokenHandler tokenHandler = new();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                !jwtSecurityToken.Header.Alg
+                .Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase)) ;
+            throw new SecurityTokenException("Token bulunamadı.");
+
+            return principal;
         }
+
+   
     }
 }
